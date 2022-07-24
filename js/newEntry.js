9690f270a76db06877788729c175c92442ce4848
JuanJacoboViera <juanjacoboviera@gmail.com>
2022-07-24 18:25:42 -0500

    add entry registration and form validation

--- js/newEntry.js
+++ js/newEntry.js
@@ -1,8 +1,9 @@
 // variables
-
+const incomeOption = document.querySelector("#income");
+const expenseOption = document.querySelector("#expense");
 const categories = document.querySelectorAll(".categories");
 const radios = document.querySelectorAll(".radio");
-let entryType = "income";
+let entryType 
 let categoryValue 
 const submitEntryBtn = document.querySelector(".createEntryBtn");
 let entriesLog = []
@@ -29,15 +30,58 @@ const createEntry = () =>{
 
     const entry = new Entry(type, category, description, amount, date);
     entriesLog.push(entry);
-
 }
 
+const clearForm = () =>{
+    document.querySelector(".entry__form").reset()
+    document.querySelector("#categoryTitle").textContent = "Category"
+    document.querySelector("#incomeCategory").setAttribute("disabled", "disabled");
+    document.querySelector("#expenseCat").classList.add("hideCategory");
+    document.querySelector("#incomeCat").classList.remove("hideCategory");
+    categoryValue = undefined
+   
+}
+
+const approvedEntryMsg =  () =>{
+    Toastify({
+        text: "Entry Registered Successfully",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "#7f42ff",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+}
+
+
+const failedEntryMsg =  () =>{
+    Toastify({
+        text: "You must fill in all inputs",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "rgb(244, 89, 89)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+}
 
 // event listeners
 
 for (const radio of radios) {
-    const incomeOption = document.querySelector("#income");
-    const expenseOption = document.querySelector("#expense");
+    // const incomeOption = document.querySelector("#income");
+    // const expenseOption = document.querySelector("#expense");
     const incomeCategory = document.querySelector("#incomeCat");
     const expenseCategory = document.querySelector("#expenseCat");
     radio.onclick = (e) =>{
@@ -45,6 +89,8 @@ for (const radio of radios) {
             expenseOption.checked = false;
             expenseCategory.classList.add("hideCategory");
             incomeCategory.classList.remove("hideCategory");
+            document.querySelector("#incomeCategory").removeAttribute("disabled");
+            document.querySelector("#categoryTitle").textContent = "Income Category"
         } else if(e.target.value === "expense"){
             incomeCategory.classList.add("hideCategory");
             expenseCategory.classList.remove("hideCategory");
@@ -64,8 +110,16 @@ categories.forEach(el =>{
 })
 
 submitEntryBtn.addEventListener("click", (e)=>{
-    e.preventDefault()
-    createEntry()
+    e.preventDefault();
+    if(((expenseOption.checked = false) && (incomeOption.checked = false)) || categoryValue === undefined || document.querySelector("#description").value.length === 0 || parseFloat(document.querySelector("#amount").value === isNaN()) || document.querySelector("#amount").value === '' || document.querySelector("#date2").value === ""){
+        failedEntryMsg()
+        console.log("failed")
+    }else{
+    console.log("approved")
+    createEntry();
+    approvedEntryMsg()
+    clearForm();
+    }
 
 })
 
