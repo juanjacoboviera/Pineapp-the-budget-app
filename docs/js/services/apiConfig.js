ccbe3a2be3d1239eb10e9e66592da0888ab22213
JuanJacoboViera <juanjacoboviera@gmail.com>
2025-04-05 12:13:17 -0500

    add render.com url to API URL

--- docs/js/services/apiConfig.js
+++ docs/js/services/apiConfig.js
@@ -1,6 +1,6 @@
 
 const local = 'http://localhost:3000'
-const production = 'https://pineapp-the-budget-app.vercel.app/api'
+const production = 'https://pineapp-the-budget-app.onrender.com'
 const API_URL = production
 
 export { API_URL };
